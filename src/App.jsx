2a19e46e73d48fff0ff033685485cf8c6bf5d92f
TheDarkLady <lakshmichaithanya13@gmail.com>
2024-10-12 00:08:50 +0530

    total Amt added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Heading from './components/Heading'
-
+import Total_amount from './components/Total_amount'
 function App() {
 
   return (
     <>
       <Heading />
+
+      <Total_amount />
     </>
   )
 }
---
+++ src/components/Total_amount.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Total_amount() {
+  return (
+    <div>
+      <h1>Total Amount</h1>
+    </div>
+  )
+}
