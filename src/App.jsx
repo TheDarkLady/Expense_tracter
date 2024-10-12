0c4911624663d1b49cf615ae69acecb815eb26d8
TheDarkLady <lakshmichaithanya13@gmail.com>
2024-10-12 23:48:51 +0530

    hlf completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import IncomeExpense from './components/IncomeExpense'
 import TransactionsList from './components/transactionsList'
 import AddTransaction from './components/AddTransaction'
 import './App.css'
+import { GlobalProvider } from './context/GlobalState'
 function App() {
 
   return (
-    <>
+    <GlobalProvider>
       <Heading />
       <div className="comtainer">
         <Balance />
@@ -16,7 +17,7 @@ function App() {
         <TransactionsList />
         <AddTransaction />
       </div>
-    </>
+    </GlobalProvider>
   )
 }
 
