d62e8573560aebc7e91014d6fe20995ab8137393
TheDarkLady <lakshmichaithanya13@gmail.com>
2024-10-13 19:30:57 +0530

    basic complete

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,12 +1,23 @@
-import React, { useState } from 'react'
-
+import React, { useContext, useState } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 function AddTransaction() {
     const [text, setText] = useState('')
     const [amount, setAmount] = useState(0)
+    const {addTransaction} = useContext(GlobalContext)
+
+    function onSubmit(e) {
+        e.preventDefault()
+        const newTransaction = {
+            id: Math.floor(Math.random() * 100000000),
+            text,
+            amount: +amount
+        }
+        addTransaction(newTransaction)
+    }
   return (
     <>
       <h3>Add new transaction</h3>
-      <form id="form">
+      <form id="form" onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text"value={text} onChange={(e) => setText(e.target.value)} id="text" placeholder="Enter text..." />
--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-
-function Transaction({transaction}) {
-    const sign = transaction.amount < 0 ? '-' : '+'
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+function Transaction({ transaction }) {
+  const {deleteTransaction} = useContext(GlobalContext)
+  const sign = transaction.amount < 0 ? "-" : "+";
   return (
-    <li key={transaction.id} className={transaction.amount < 0 ? 'minus' : 'plus'}>
-            {transaction.text} <span>{}{sign}${Math.abs(transaction.amount)}</span>
-          </li>
-  )
+    <li
+      key={transaction.id}
+      className={transaction.amount < 0 ? "minus" : "plus"}
+    >
+      {transaction.text}{" "}
+      <span>
+        {}
+        {sign}${Math.abs(transaction.amount)}
+      </span>
+      <button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>
+        x
+      </button>
+    </li>
+  );
 }
 
-export default Transaction
+export default Transaction;
